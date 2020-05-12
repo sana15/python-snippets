339411303abb5466a71ca41aa505f14ef83684fa
sana <ssanasultana15@gmail.com>
2020-05-12 17:20:23 +0530

    Number off folders in a directory or folder

---
+++ countfiles.py
@@ -0,0 +1,7 @@
+################count files in a folder #############
+import os #########os module
+pathOfFolder = /Desktop/
+count = 0
+for file in os.listdir(path):
+	count = count+1
+print(count)
