0cfd18d11fa5d0de845d896b500a206a2a503635
sana15 <ssanasultana15@gmail.com>
2020-07-07 23:04:55 +0530

    as function

--- countfiles.py
+++ countfiles.py
@@ -1,7 +1,9 @@
 ################count files in a folder #############
 import os #########os module
 pathOfFolder = /Desktop/
-count = 0
-for file in os.listdir(path):
-	count = count+1
-print(count)
+# count = 0
+def CountFiles(path):
+	count = 0
+	for file in os.listdir(path):
+		count = count+1
+	print(count)
